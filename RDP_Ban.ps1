423fb3716cf13bfd95e4e30e79d433ca52cb3936
aelrou <mike@aelrou.net>
2020-04-01 01:45:59 -0600

    Code cleanup

--- RDP_Ban.ps1
+++ RDP_Ban.ps1
@@ -1,39 +1,28 @@
 # Set-ExecutionPolicy RemoteSigned
+# "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\Users\Public\PowerShell\RDP_Ban.ps1"
 $Store = "C:\Users\Public\PowerShell\RDP_Ban"
 $WatchList = "$($Store)\WatchList"
-$FormatTime = "yyyy-MM-ddTHH:mm:ss.ffff"
 
 $SecurityLog = Get-WinEvent -FilterHashtable @{LogName = "Security"; Id = 4625 } -MaxEvents 1
 $SecurityLogXML = [xml]$SecurityLog.ToXml()
-
-$MachineName = $SecurityLog.MachineName
-# $LogName = $SecurityLog.LogName
-# $KeywordsDisplayNames = $SecurityLog.KeywordsDisplayNames
-# $EventId = $SecurityLog.Id
-$RecordId = $SecurityLog.RecordId
+$FormatTime = "yyyy-MM-ddTHH:mm:ss.ffff"
 $TimeCreated = (Get-Date -Date $SecurityLog.TimeCreated -Format $FormatTime)
 $IpAddress = ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "IpAddress" })."#text"
-$WorkstationName = ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "WorkstationName" })."#text"
-$TargetDomainName = ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "TargetDomainName" })."#text"
-$TargetUserName = ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "TargetUserName" })."#text"
 
 $NewEvent = $null
 $NewEvent = @{ }
-$NewEvent.Add("MachineName", $MachineName)
-# $NewEvent.Add("LogName", $LogName)
-# $NewEvent.Add("KeywordsDisplayNames", $KeywordsDisplayNames)
-# $NewEvent.Add("EventId", # $EventId)
-$NewEvent.Add("RecordId", $RecordId)
+$NewEvent.Add("MachineName", $SecurityLog.MachineName)
+$NewEvent.Add("RecordId", $SecurityLog.RecordId)
 $NewEvent.Add("TimeCreated", $TimeCreated)
 $NewEvent.Add("IpAddress", $IpAddress)
-$NewEvent.Add("WorkstationName", $WorkstationName)
-$NewEvent.Add("TargetDomainName", $TargetDomainName)
-$NewEvent.Add("TargetUserName", $TargetUserName)
+$NewEvent.Add("WorkstationName", ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "WorkstationName" })."#text")
+$NewEvent.Add("TargetDomainName", ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "TargetDomainName" })."#text")
+$NewEvent.Add("TargetUserName", ($SecurityLogXML.Event.EventData.Data | Where-Object { $_.Name -eq "TargetUserName" })."#text")
 
 $Events = $null
 $Events = @{ }
-foreach ($pair in $NewEvent) {
-    $Events.Add($TimeCreated, $pair)
+foreach ($Pair in $NewEvent) {
+    $Events.Add($TimeCreated, $Pair)
 }
 
 if (!(Test-Path -Path "$($WatchList)\$($IpAddress).json" -PathType Leaf)) {
@@ -42,21 +31,21 @@ if (!(Test-Path -Path "$($WatchList)\$($IpAddress).json" -PathType Leaf)) {
     }
 }
 else {
-    $StoredData = (Get-Content -Path "$($WatchList)\$($IpAddress).json" | ConvertFrom-Json)
-    $StoredData.PSObject.Properties | ForEach-Object { $Events[$_.Name] = $_.Value }
+    $StoredEvents = (Get-Content -Path "$($WatchList)\$($IpAddress).json" | ConvertFrom-Json)
+    $StoredEvents.PSObject.Properties | ForEach-Object { $Events[$_.Name] = $_.Value }
 }
 
 $EventsToRemove = $null
 $EventsToRemove = @{ }
-foreach ($pair in $Events.GetEnumerator()) {
-    $EventTime = [datetime]::ParseExact($pair.Value.TimeCreated, $FormatTime, $null)
+foreach ($Pair in $Events.GetEnumerator()) {
+    $EventTime = [datetime]::ParseExact($Pair.Value.TimeCreated, $FormatTime, $null)
     if ($EventTime -lt (Get-Date).AddMinutes(-10)) {
-        $EventsToRemove.Add($pair.Name, $null)
+        $EventsToRemove.Add($Pair.Name, $null)
     }
 }
 
-foreach ($pair in $EventsToRemove.GetEnumerator()) {
-    $Events.Remove($pair.Name)
+foreach ($Pair in $EventsToRemove.GetEnumerator()) {
+    $Events.Remove($Pair.Name)
 }
 
 $BanList = $null
@@ -64,33 +53,29 @@ $BanList = @{ }
 if ($Events.Count -gt 10) {
     $BanList.Add($IpAddress, (Get-Date -Format $FormatTime))
     if (Test-Path -Path "$($Store)\BanList.json" -PathType Leaf) {
-        $StoredBanData = (Get-Content -Path "$($Store)\BanList.json" | ConvertFrom-Json)
-        $StoredBanData.PSObject.Properties | ForEach-Object { $BanList[$_.Name] = $_.Value }
+        $StoredBanList = (Get-Content -Path "$($Store)\BanList.json" | ConvertFrom-Json)
+        $StoredBanList.PSObject.Properties | ForEach-Object { $BanList[$_.Name] = $_.Value }
     }
 
-    $i = 0
-    foreach ($pair in $BanList.GetEnumerator()) {
-        if ($i -lt 1) {
-            $BanListString = $pair.Name
+    $First = $true
+    foreach ($Pair in $BanList.GetEnumerator()) {
+        if ($First) {
+            $BanListString = $Pair.Name
+            $First = $false
         }
         else {
-            $BanListString = "$($BanListString),$($pair.Name)"
+            $BanListString = "$($BanListString),$($Pair.Name)"
         }
-        $i++
     }
 
     $RuleNameTCP = "RDP_Ban - TCP 3389"
     $RuleNameUDP = "RDP_Ban - UDP 3389"
     
     Start-Process -WorkingDirectory "C:\Windows\System32" -NoNewWindow -Wait -FilePath "C:\Windows\System32\netsh.exe" -ArgumentList "advfirewall", "firewall", "delete", "rule", "name=""$($RuleNameTCP)""" # -RedirectStandardOutput "$($Store)\stdout.log" -RedirectStandardError "$($Store)\stderr.log" -ErrorAction Stop
-    # Write-Host (Get-Content -Path "$($Store)\stdout.log")
     Start-Process -WorkingDirectory "C:\Windows\System32" -NoNewWindow -FilePath "C:\Windows\System32\netsh.exe" -ArgumentList "advfirewall", "firewall", "add", "rule", "name=""$($RuleNameTCP)""", "dir=in", "action=block", "enable=yes", "profile=any", "protocol=tcp", "localport=3389", "remoteip=$($BanListString)" # -RedirectStandardOutput "$($Store)\stdout.log" -RedirectStandardError "$($Store)\stderr.log" -ErrorAction Stop
-    # Write-Host (Get-Content -Path "$($Store)\stdout.log")
     
     Start-Process -WorkingDirectory "C:\Windows\System32" -NoNewWindow -Wait -FilePath "C:\Windows\System32\netsh.exe" -ArgumentList "advfirewall", "firewall", "delete", "rule", "name=""$($RuleNameUDP)""" # -RedirectStandardOutput "$($Store)\stdout.log" -RedirectStandardError "$($Store)\stderr.log" -ErrorAction Stop
-    # Write-Host (Get-Content -Path "$($Store)\stdout.log")
     Start-Process -WorkingDirectory "C:\Windows\System32" -NoNewWindow -FilePath "C:\Windows\System32\netsh.exe" -ArgumentList "advfirewall", "firewall", "add", "rule", "name=""$($RuleNameUDP)""", "dir=in", "action=block", "enable=yes", "profile=any", "protocol=udp", "localport=3389", "remoteip=$($BanListString)" # -RedirectStandardOutput "$($Store)\stdout.log" -RedirectStandardError "$($Store)\stderr.log" -ErrorAction Stop
-    # Write-Host (Get-Content -Path "$($Store)\stdout.log")
 }
 
 if ($BanList.Count -gt 0) {
@@ -108,4 +93,4 @@ else {
     }    
 }
 
-Write-host "-------------------------------------"
+Write-host "-----"
